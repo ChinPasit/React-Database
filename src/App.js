2e9e0afee985e9b5b5f1551be7d941bdaebe6670
Sudatip <5910110216@psu.ac.th>
2019-04-04 00:01:11 +0700

    Finish

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+//import {StyleSheet,View} from 'react-native';
+var config = {
+  apiKey: "AIzaSyALI99GmjUB8D54YFKChBvrShrnb7yYT7I",
+  authDomain: "react-chapter8.firebaseapp.com",
+  databaseURL: "https://react-chapter8.firebaseio.com",
+  projectId: "react-chapter8",
+  storageBucket: "react-chapter8.appspot.com",
+  messagingSenderId: "228016244388"
+};
+firebase.initializeApp(config);
+
+var db=firebase.database();
 
 class App extends Component {
+  state = {pets:[
+    {name : 'Chin', type:'Chin'}
+  ]}
+  componentDidMount(){
+    db.ref('/แมว-สายพันธุ์').on('value',snapshot => {
+      let val =snapshot.val();
+      this.setState({pets:val})
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      
+      <div>
+        {this.state.pets.map(it => <div>{it.ชื่อสายพันธุ์}</div>)} 
+      </div> 
+      
     );
   }
 }
+/*const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#F5FCFF',
+  },
+  ctext:{
+    fontSize:20,
+    textAlign:'center',
+    margin:10,
+  },
+});*/
 
 export default App;
